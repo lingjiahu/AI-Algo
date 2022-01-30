e57ce5eadf1c15c270ea087279d637771b7c4803
lingjiahu <lingjiahu@outlook.com>
2022-01-30 12:56:57 -0500

    ma

--- src/A1/TSP.java
+++ src/A1/TSP.java
@@ -1,6 +1,7 @@
 package A1;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Random;
 
@@ -34,6 +35,7 @@ public class TSP {
     // visit all cities in a random order, return cost
     public double randomTour(int numCities) {
         // TODO
+//        Collections.shuffle(this.cities);
         Random random = new Random();
         ArrayList<City> remainCities = new ArrayList<>();
         for (int i = 0; i < numCities; i++) {
@@ -52,23 +54,39 @@ public class TSP {
 
     // search for optimal route by hill climbing, return the cost of the shortest one
     public double HillClimbingOptimalTour() {
-        double minCost = Double.MAX_VALUE;
-        boolean flg = true;
         ArrayList<City> curBest = this.randomTour;
-        while (flg) {
-            flg = false;
-            ArrayList<ArrayList<City>> neighbours = getNeighbours(curBest);
-            for (ArrayList<City> tour : neighbours) {
-                double tourCost = costOfTour(tour);
-                if (tourCost < minCost) {
-                    flg = true;
-                    minCost = tourCost;
-                    curBest = tour;
+        double bestCost = costOfTour(curBest);
+
+        ArrayList<City> next_best_route=get_best(curBest);
+        double current_cost=costOfTour(next_best_route);
+
+        while (current_cost < bestCost) {
+            bestCost=current_cost;
+            next_best_route=get_best(curBest);
+            current_cost=costOfTour(next_best_route);
+
+
+        }
+        return bestCost;
+    }
+
+    public ArrayList<City>  get_best(ArrayList<City> route) {
+        double minCost = costOfTour(route);
+        ArrayList<City> curBest=route;
+
+        for (int i  = 0; i < route.size(); i++) { // i < j
+            for (int j = 0; j < route.size() && j != i; j++) {
+                ArrayList<City> neighbour = swap_2(curBest, i, j);
+                double curCost = costOfTour(neighbour);
+                if (curCost < minCost) {
+                    minCost = curCost;
+                    curBest = neighbour;
                 }
             }
         }
-        return minCost;
+        return curBest;
     }
+
 
     // return cost of a tour
     public double costOfTour(ArrayList<City> pTour) {
@@ -97,25 +115,79 @@ public class TSP {
     //    3. take route[k+1] to end and add them in order to new_route
     //    return new_route;
     //}
-    public ArrayList<ArrayList<City>> getNeighbours(ArrayList<City> curTour) {
-        ArrayList<ArrayList<City>> neighbours = new ArrayList<>();
-        for (int i  = 0; i < curTour.size() - 1; i++) { // i < j
-            for (int j = i+1; j < curTour.size(); j++) {
-                ArrayList<City> permutedTour = new ArrayList<>();
-                for (int k = 0; k <= i -1; k++) {
-                    permutedTour.add(curTour.get(i));
-                }
-                for (int k = j; k >= i; k--) {
-                    permutedTour.add(curTour.get(k));
-                }
+//    public ArrayList<ArrayList<City>> getNeighbours(ArrayList<City> curTour) {
+//        ArrayList<ArrayList<City>> neighbours = new ArrayList<>();
+//        for (int i  = 0; i < curTour.size() - 1; i++) { // i < j
+//            for (int j = i+1; j < curTour.size() && j != i; j++) {
+//                ArrayList<City> permutedTour = new ArrayList<>();
+//                for (int k = 0; k <= i -1; k++) {
+//                    permutedTour.add(curTour.get(i));
+//                }
+//                for (int k = j; k >= i; k--) {
+//                    permutedTour.add(curTour.get(k));
+//                }
+//                for (int k = j+1; k < curTour.size(); k++) {
+//                    permutedTour.add(curTour.get(k));
+//                }
+//                neighbours.add(permutedTour);
+//            }
+//        }
+//        return neighbours;
+//    }
 
-                for (int k = j+1; k < curTour.size(); k++) {
-                    permutedTour.add(curTour.get(k));
-                }
-                neighbours.add(permutedTour);
-            }
+//    public ArrayList<City> getNeighbours(ArrayList<City> curTour, int i, int j) {
+//                ArrayList<City> permutedTour = new ArrayList<>();
+//                ArrayList<City> permutedTour1 = new ArrayList<>();
+//                for (int k = 0; k < i; k++) {
+//                    permutedTour.add(curTour.get(k));
+//                }
+//                for (int k = i; k <= j; k++) {
+//                    permutedTour1.add(curTour.get(k));
+//                }
+//                Collections.reverse(permutedTour1);
+//                permutedTour.addAll(permutedTour1);
+//                for (int k = j+1; k < curTour.size(); k++) {
+//                    permutedTour.add(curTour.get(k));
+//                }
+//        return permutedTour;
+//    }
+
+    public ArrayList<City> swap_2(ArrayList<City> all_cities, int swap1, int swap2){
+        if (swap1 > swap2) {
+            int temp = swap2;
+            swap2 = swap1;
+            swap1 = temp;
         }
-        return neighbours;
+        int total=all_cities.size();
+
+        //copy 0 to swap1 not included
+        ArrayList<City> route1=new ArrayList<>();
+        for (int i=0;i<swap1;i++){
+            route1.add(all_cities.get(i));
+        }
+
+        //copy swap 1 to swap 2 both included
+        ArrayList<City> route2=new ArrayList<>();
+        for (int i=swap1;i<=swap2;i++){
+            route2.add(all_cities.get(i));
+        }
+
+        //swap them by reverse order
+        Collections.reverse(route2);
+
+        //copy swap2 (not included) to end
+        ArrayList<City> route3=new ArrayList<>();
+        for (int i=swap2+1;i<total;i++){
+            route3.add(all_cities.get(i));
+        }
+
+        //merge 2 together
+        route1.addAll(route2);
+        route1.addAll(route3);
+
+        //return copy
+        return route1;
+
     }
 
     // construct distance table
